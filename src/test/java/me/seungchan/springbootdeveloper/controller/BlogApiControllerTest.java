cd77b48018027e7dc4c147f34aca168dd17fe707
dmdrk1414 <seungchan141414@gmail.com>
2023-06-28 08:30:27 +0900

    [ADD] BlogApiController에 대한 Test 요청 확인
           // when
            // 성정한 내용을 바탕으로 요청 전송
            ResultActions result = mockMvc.perform(post(url) // 블로그 글 추가에 필요한 요청 객체를 만든다.
                    .contentType(MediaType.APPLICATION_JSON_VALUE) // 쵸청 타입은 JSON입니다.
                    .content(requestBody)); // given 절에서 미리 만들어둔 객체를 요청 본문으로 함께 보냅니다.

--- src/test/java/me/seungchan/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/me/seungchan/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -1,17 +1,28 @@
 package me.seungchan.springbootdeveloper.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.seungchan.springbootdeveloper.domain.Article;
+import me.seungchan.springbootdeveloper.dto.AddArticleRequest;
 import me.seungchan.springbootdeveloper.repository.BlogRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.assertj.core.api.Assertions.assertThat;
 
 // 메인 애플리케이션 클래스에 추가하는 애너테이션인 @SpringBootApplication이 있는 클래스 찾고
 // 클래스에 포함되어 있는 빈을 찾은 다음, 테스트용 애플리케이션 컨텍스트라는 것을 만든다.
@@ -34,9 +45,38 @@ class BlogApiControllerTest {
     BlogRepository blogRepository;
 
     @BeforeEach // 테스트 실행 전 실행하는 메서드
-    public void mockMvcSetUp(){
+    public void mockMvcSetUp() {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(context) // MockMVB 설정
                 .build();
         blogRepository.deleteAll();
+    }
+
+    @DisplayName("addArticle: 블로그 글추가에 성공한다. ")
+    @Test
+    public void addArticle() throws Exception {
+        // given
+        // 블로그 글추가에 필요한 요청 객체 만들기
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+        final AddArticleRequest userRequest = new AddArticleRequest(title, content);
+
+        // 객체 JSON 으로 직렬화
+        final String requestBody = objectMapper.writeValueAsString(userRequest); // 객체의 직렬화
+
+        // when
+        // 성정한 내용을 바탕으로 요청 전송
+        ResultActions result = mockMvc.perform(post(url) // 블로그 글 추가에 필요한 요청 객체를 만든다.
+                .contentType(MediaType.APPLICATION_JSON_VALUE) // 쵸청 타입은 JSON입니다.
+                .content(requestBody)); // given 절에서 미리 만들어둔 객체를 요청 본문으로 함께 보냅니다.
+
+        // then
+        result.andExpect(status().isCreated());
+
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles.size()).isEqualTo(1); // 크기가 1인지 검증
+        assertThat(articles.get(0).getTitle()).isEqualTo(title); // article의 제목이 title과 같은지 확인
+        assertThat(articles.get(0).getContent()).isEqualTo(content); // article의 내용이 content과 같은지 확인
     }
 }
