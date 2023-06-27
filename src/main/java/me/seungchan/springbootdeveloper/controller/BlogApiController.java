b563416383b819472f6f580d46eca0b872b17fea
dmdrk1414 <seungchan141414@gmail.com>
2023-06-27 21:13:56 +0900

    [ADD] BlogApiController 작성
    /api/articles에 POST 요청이 오면 @PostMapping을 이용해 요청을 매핑한 뒤
    블로그 글을 생성하는 BlogService의 save() 메서드를 호출한 뒤
    생성된 블로그 글을 반환하는 작업을 할 addArticle()메서드를 작성하겠습니다.

---
+++ src/main/java/me/seungchan/springbootdeveloper/controller/BlogApiController.java
@@ -0,0 +1,30 @@
+package me.seungchan.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.seungchan.springbootdeveloper.domain.Article;
+import me.seungchan.springbootdeveloper.dto.AddArticleRequest;
+import me.seungchan.springbootdeveloper.service.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor // final 이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 룸복 어노테이션
+@RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
+public class BlogApiController {
+    // TODO : 빈주신 삭제
+    @Autowired
+    private final BlogService blogService;
+
+    // HTTP 메서드가 POST일 때 전달 받은 URL와 동일하면 메서드로 매핑
+    @PostMapping("/api/articles")
+    // 요청 본문 값 매핑
+    public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
+        Article savedArticle = blogService.save(request);
+
+        // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+}
