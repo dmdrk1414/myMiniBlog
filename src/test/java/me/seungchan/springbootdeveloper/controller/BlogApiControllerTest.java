bdfea6d887b89b597b29965e8c3e60c2f4304d01
dmdrk1414 <seungchan141414@gmail.com>
2023-06-28 21:40:50 +0900

    [Test] indAllArticles: 블로그 글 목록 조회에 성공한다.
    요청에 응응답하여 원하는 값을 가져오는 것

--- src/test/java/me/seungchan/springbootdeveloper/controller/BlogApiControllerTest.java
+++ src/test/java/me/seungchan/springbootdeveloper/controller/BlogApiControllerTest.java
@@ -20,7 +20,9 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -78,5 +80,34 @@ class BlogApiControllerTest {
         assertThat(articles.size()).isEqualTo(1); // 크기가 1인지 검증
         assertThat(articles.get(0).getTitle()).isEqualTo(title); // article의 제목이 title과 같은지 확인
         assertThat(articles.get(0).getContent()).isEqualTo(content); // article의 내용이 content과 같은지 확인
+    }
+
+
+    @DisplayName("findAllArticles: 블로그 글 목록 조회에 성공한다.")
+    @Test
+    public void findAllArticles() throws Exception{
+        //given
+        // 블로그 글 관련 정보들을 저장
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+
+        blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        //when
+        // 목록 조회 API 호출
+        final ResultActions resultActions = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        //then
+        // 0번 인덱스 요소중
+        // content와 title이 저장된 값과 같은지 확인
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value(content))
+                .andExpect(jsonPath("$[0].title").value(title));
     }
 }
