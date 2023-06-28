2e51e948fedc9544980b8c9d1dda5a47dd704de9
dmdrk1414 <seungchan141414@gmail.com>
2023-06-28 17:48:58 +0900

    [ADD] ArticleResponse 클래스는 DTO이다. 엔티티를 인수로 받는 생성자를 추가. 코드 수정

--- src/main/java/me/seungchan/springbootdeveloper/dto/ArticleResponse.java
+++ src/main/java/me/seungchan/springbootdeveloper/dto/ArticleResponse.java
@@ -1,6 +1,7 @@
 package me.seungchan.springbootdeveloper.dto;
 
 import lombok.Getter;
+import me.seungchan.springbootdeveloper.domain.Article;
 
 
 // 응답을 위한 DTO를 먼저 작성
@@ -13,8 +14,8 @@ public class ArticleResponse {
 //    글은 제목과 내용 구성이므로
 //    해당 필드를 가지는 클래스를 만든 다음
 //    엔티티를 인수로 받는 생성자를 추가.
-    public ArticleResponse(String title, String content) {
-        this.title = title;
-        this.content = content;
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
     }
 }
