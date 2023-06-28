eb7a7ec0aaff3576f23c569cde0a4410243961f9
dmdrk1414 <seungchan141414@gmail.com>
2023-06-28 16:49:30 +0900

    [ADD] BlogService 에서 블로그 글 조회을 하여 반환하는 메서드

--- src/main/java/me/seungchan/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/seungchan/springbootdeveloper/service/BlogService.java
@@ -7,6 +7,8 @@ import me.seungchan.springbootdeveloper.repository.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor // final이 붙거나  @NotNull이 붙는 필드의 생성자 추가
 @Service  // 빈으로 등록
 public class BlogService {
@@ -20,5 +22,10 @@ public class BlogService {
         // controller에서 받은 데이터를
         // blogRepository에 의해 DB로 저장한다.
         return blogRepository.save((request.toEntity()));
+    }
+
+    // 블로그 글 조회 메서드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
