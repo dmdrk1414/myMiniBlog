0fbf1cc9634cca84b7f52a09fff116cc86f16be7
dmdrk1414 <seungchan141414@gmail.com>
2023-06-30 09:51:59 +0900

    [주석 ADD] 컨트롤러에 주석을 추가
    꼭 알아두면 좋을 응답 코드
    • 200OK : 요청이 성공적으로 수행되었음
    • 201 Created : 요청이 성공적으로 수행되었고, 새로운 리소스가 생성되었음
    • 400 Bad Request : 요청 값이 잘못되어 요청에 실패했음
    • 403 Forbidden : 권한이 없어 요청에 실패했음 .
    • 404 Not Found : 요청 값으로 찾은 리소스가 없어 요청에 실패했음
    • 500 Internal Server Error : 서버 상에 문제가 있어 요청에 실패했음

--- src/main/java/me/seungchan/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/seungchan/springbootdeveloper/controller/BlogApiController.java
@@ -13,7 +13,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// 컨트롤러 계층 설명
+// 컨트롤러 메서드에는 URL 매핑 애너테이션
+// @GetMapping, @PostMapping, @PutMapping, @DeleteMapping 을 사용
+
 @RequiredArgsConstructor // final 이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 룸복 어노테이션
+
+// @RestController
+// HTTP 응답으로 객체 데이터를 JSON 형식으로 반환
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
 public class BlogApiController {
     // TODO : 빈주신 삭제
@@ -23,13 +30,15 @@ public class BlogApiController {
     // HTTP 메서드가 POST일 때 전달 받은 URL와 동일하면 메서드로 매핑
     @PostMapping("/api/articles")
     // 요청 본문 값 매핑
+    // @RequestBody 애너테이션은 HTTP를 요청할 때 응답에 해당하는 값을
+    // @RequestBody 애너테이션이 붙은 대상 객체인 AddArticleRequest에 매핑한다.
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article savedArticle = blogService.save(request);
 
         // 요청한 자원이 성공적으로 생성되었으며 저장된 블로그 글 정보를 응답 객체에 담아 전송
         // 요청한 정보를 로직처리를 하여 리턴을 한다.
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(savedArticle);
+        return ResponseEntity.status(HttpStatus.CREATED) // 응답 코드로 201, Created를 응답하고
+                .body(savedArticle); // 테이블에 저장된 객체를 반환
     }
 
     // HTTP 메서드가 GET일 때 전달 받는 URL와 동일하면 메서드로 매핑
