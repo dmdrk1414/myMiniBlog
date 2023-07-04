4d8f66db538b9b38f23d42a46f25218aaf53bee3
dmdrk1414 <seungchan141414@gmail.com>
2023-07-04 22:56:01 +0900

    UserDetailService 스프링 시큐리티에서 로그인을 진행할 때 사용자 정보를 가져오는 코드를 작성

---
+++ src/main/java/me/seungchan/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,25 @@
+package me.seungchan.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.seungchan.springbootdeveloper.domain.User;
+import me.seungchan.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+// 스프링 시큐리티에서 사용자의 정보를 가져오는 UserDetailsService 인터페이스를 구현합니다.
+// 필수로 구현해야 하는 loadUserByUsername을 구현
+
+// 스프링 시큐리티에서 로그인을 진행할 때 사용자 정보를 가져오는 코드를 작성
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // user의 이름(email)을 이용해 유저의 정보를 가져온다.
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
