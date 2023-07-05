a285723bd0ba641afb6843bd31ff4c0d5a14081e
dmdrk1414 <seungchan141414@gmail.com>
2023-07-05 13:55:19 +0900

    View 관련 로그인, 회원가입 url

--- src/main/java/me/seungchan/springbootdeveloper/controller/UserViewController.java
+++ src/main/java/me/seungchan/springbootdeveloper/controller/UserViewController.java
@@ -1,13 +1,23 @@
 package me.seungchan.springbootdeveloper.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserViewController {
-
+    // /login 경로로 접근하면 login() 메서드가 login.html
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    // /signup 경로에 접근하면 signup() 메서드는 singnup.html를 반환
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
     }
 }
