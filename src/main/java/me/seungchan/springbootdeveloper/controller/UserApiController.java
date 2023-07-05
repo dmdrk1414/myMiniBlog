7b4dd61879a91ca55e47e7f1499e2831dd5b601e
dmdrk1414 <seungchan141414@gmail.com>
2023-07-05 14:07:39 +0900

    user api controller 부분 유저와 로그아웃 컨트롤러 추가

--- src/main/java/me/seungchan/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/me/seungchan/springbootdeveloper/controller/UserApiController.java
@@ -15,14 +15,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserApiController {
 
+    private final UserService userService;
+
     // 회원 가입 처리가 된 다음 로그인 페이지로 이동하기 위해
     // redirect: 접두사를 붙였다.
     // 이렇게 하면 회원 가입 끝나면 강제로 /login URL에 해당하는 화면으로 이동한다.
-    private final UserService userService;
-
     @PostMapping("/user")
     public String signup(AddUserRequest request) {
         userService.save(request); // 회원 가입 메서드 호출
         return "redirect:/login"; // 회원 가입이 완료된 이후에 로그인 페이지로 이동
+    }
+
+    // /logout GET 요청을 하면 로그아웃을 담당하는 핸들러인 SecurityContextLogoutHandler의
+    // logout() 메서드를 호출해서 로그아웃한다.
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response
+                , SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
