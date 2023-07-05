f9e3faa8b04fc523fd4c11843a7b34d7d1f7915e
dmdrk1414 <seungchan141414@gmail.com>
2023-07-05 16:09:52 +0900

    HTML의 폼에서 데이터를 얻는 곳은 set을 만들어야 한다.

--- src/main/java/me/seungchan/springbootdeveloper/dto/AddUserRequest.java
+++ src/main/java/me/seungchan/springbootdeveloper/dto/AddUserRequest.java
@@ -2,10 +2,14 @@ package me.seungchan.springbootdeveloper.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 // 사용자의 정보를 담고 있는 객체를 작성
 @NoArgsConstructor
 @Getter
+@Setter
+@ToString
 public class AddUserRequest {
     private String email;
     private String password;
